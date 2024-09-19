6b7b797a9fc4bbac6f6953c8f1cf619da4b380a9
Pavlo <pavlonedaskivskij8@gmail.com>
2024-09-19 18:11:43 +0300

    init

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "MainLayout"
+}
+</script>
+
+<template>
+    <div class="w-96 mx-auto pb-10">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,19 @@
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+export default {
+    name: "Index",
+    components: {MainLayout}
+}
+</script>
+
+<template>
+    <MainLayout>
+
+        <h1 class="text-lg">Index post</h1>
+    </MainLayout>
+</template>
+
+<style scoped>
+
+</style>
